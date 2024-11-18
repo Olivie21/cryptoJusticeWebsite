d441808369fe8d2b869affd95f7cef61dbccd818
Olivie21 <oliver.young7@outlook.com>
2024-11-18 21:15:59 +0700

    Update MobileMenu.js

--- components/MobileMenu/MobileMenu.js
+++ components/MobileMenu/MobileMenu.js
@@ -1,205 +1,144 @@
-import React, { Fragment, useState } from 'react';
+import React, { useState } from "react";
+import Link from "next/link";
 import Collapse from "@mui/material/Collapse";
 import ListItem from "@mui/material/List";
 import List from "@mui/material/List";
-import Link from "next/link";
-
-const menus = [
-    {
-        id: 1,
-        title: 'Home',
-        link: '/home',
-        submenu: [
-            {
-                id: 11,
-                title: 'Home style 1',
-                link: '/home'
-            },
-            {
-                id: 12,
-                title: 'Home style 2',
-                link: '/home-2'
-            },
-            {
-                id: 13,
-                title: 'Home style 3',
-                link: '/home-3'
-            }
-        ]
-    },
-
-    {
-        id: 3,
-        title: 'Pages',
-        link: '/',
-        submenu: [
-            {
-                id: 31,
-                title: 'About Us',
-                link: '/about'
-            },
-            {
-                id: 3222,
-                title: 'Attorneys',
-                link: '/attorneys'
-            },
-            {
-                id: 322,
-                title: 'Attorneys Single',
-                link: '/attorneys-single/Willam-Stephen'
-            },
-            {
-                id: 35,
-                title: 'Login',
-                link: '/login'
-            },
-            {
-                id: 36,
-                title: 'Register',
-                link: '/register'
-            },
-        ]
-    },
-
-    {
-        id: 6,
-        title: 'Practice',
-        link: '/practice',
-        submenu: [
-            {
-                id: 61,
-                title: 'Practice',
-                link: '/practice'
-            },
-            {
-                id: 64,
-                title: 'Practice single',
-                link: '/practice-single/Family-Law'
-            },
-        ]
-    },
-    {
-        id: 7,
-        title: 'Cases',
-        link: '/case',
-        submenu: [
-            {
-                id: 71,
-                title: 'Project',
-                link: '/case'
-            },
-            {
-                id: 74,
-                title: 'Cases single',
-                link: '/case-single/Business-Accounting'
-            },
-        ]
-    },
-    {
-        id: 5,
-        title: 'Blog',
-        link: '/blog',
-        submenu: [
-            {
-                id: 51,
-                title: 'Blog',
-                link: '/blog'
-            },
-            {
-                id: 52,
-                title: 'Blog Left sidebar',
-                link: '/blog-left-sidebar'
-            },
-            {
-                id: 53,
-                title: 'Blog full width',
-                link: '/blog-fullwidth'
-            },
-            {
-                id: 54,
-                title: 'Blog single',
-                link: '/blog-single/Justice-May-For-You-If-You-Are-Innocent'
-            },
-            {
-                id: 55,
-                title: 'Blog single Left sidebar',
-                link: '/blog-single-left-sidebar/Justice-May-For-You-If-You-Are-Innocent'
-            },
-            {
-                id: 56,
-                title: 'Blog single full width',
-                link: '/blog-single-fullwidth/Justice-May-For-You-If-You-Are-Innocent'
-            },
-        ]
-    },
-    {
-        id: 88,
-        title: 'Contact',
-        link: '/contact',
-    }
-
-
-]
 
 const MobileMenu = () => {
+  const [menuActive, setMenuState] = useState(false);
+  const [openId, setOpenId] = useState(0);
 
-    const [openId, setOpenId] = useState(0);
-    const [menuActive, setMenuState] = useState(false);
+  const ClickHandler = () => {
+    window.scrollTo(10, 0);
+  };
 
-    const ClickHandler = () => {
-        window.scrollTo(10, 0);
-    }
+  const menuStructure = [
+    {
+      title: "Home",
+      link: "/",
+    },
+    {
+      title: "About",
+      link: "/about",
+    },
+    // {
+    //   title: "Cases",
+    //   submenu: [
+    //     {
+    //       title: "Cases",
+    //       link: "/case-stadies",
+    //     },
+    //     {
+    //       title: "Case Single",
+    //       link: "/case-single/Business-Accounting",
+    //     },
+    //   ],
+    // },
+    // {
+    //   title: "Blog",
+    //   submenu: [
+    //     {
+    //       title: "Blog Right Sidebar",
+    //       link: "/blog",
+    //     },
+    //     {
+    //       title: "Blog Left Sidebar",
+    //       link: "/blog-left-sidebar",
+    //     },
+    //     {
+    //       title: "Blog Fullwidth",
+    //       link: "/blog-fullwidth",
+    //     },
+    //     {
+    //       title: "Blog Details",
+    //       submenu: [
+    //         {
+    //           title: "Blog Details Right Sidebar",
+    //           link: "/blog-single/Who-Can-a-Victim-Sue-after-a-Car-Accident",
+    //         },
+    //         {
+    //           title: "Blog Details Left Sidebar",
+    //           link: "/blog-single-left-sidebar/Who-Can-a-Victim-Sue-after-a-Car-Accident",
+    //         },
+    //         {
+    //           title: "Blog Details Fullwidth",
+    //           link: "/blog-single-fullwidth/Who-Can-a-Victim-Sue-after-a-Car-Accident",
+    //         },
+    //       ],
+    //     },
+    //   ],
+    // },
+    // {
+    //   title: "Pages",
+    //   submenu: [
+    //     {
+    //       title: "Attorneys",
+    //       link: "/Attorneys",
+    //     },
+    //     {
+    //       title: "Attorney Single",
+    //       link: "/attorneys-single/Willam-Stephen",
+    //     },
+    //   ],
+    // },
+    {
+      title: "Contact",
+      link: "/contact",
+    },
+  ];
 
-    return (
-        <div className='mobail_menu'>
-            <div className={`mobileMenu ${menuActive ? "show" : ""}`}>
-                <div className="menu-close">
-                    <div className="clox" onClick={() => setMenuState(!menuActive)}><i className="ti-close"></i></div>
-                </div>
+  const renderMenuItems = (items, level = 0) => {
+    return items.map((item, index) => (
+      <ListItem key={index}>
+        {item.submenu ? (
+          <>
+            <p
+              onClick={() => setOpenId(item.title === openId ? 0 : item.title)}
+              className="flex justify-between items-center"
+            >
+              {item.title}
+              <i
+                className={
+                  item.title === openId ? "fa fa-angle-up" : "fa fa-angle-down"
+                }
+              ></i>
+            </p>
+            <Collapse in={item.title === openId} timeout="auto" unmountOnExit>
+              <List className="subMenu">
+                {renderMenuItems(item.submenu, level + 1)}
+              </List>
+            </Collapse>
+          </>
+        ) : (
+          <Link onClick={ClickHandler} href={item.link} className="block py-2">
+            {item.title}
+          </Link>
+        )}
+      </ListItem>
+    ));
+  };
 
-                <ul className="responsivemenu">
-                    {menus.map((item, mn) => {
-                        return (
-                            <ListItem className={item.id === openId ? 'active' : null} key={mn}>
-                                {item.submenu ?
-                                    <Fragment>
-                                        <p onClick={() => setOpenId(item.id === openId ? 0 : item.id)}>{item.title}
-                                            <i className={item.id === openId ? 'fa fa-angle-up' : 'fa fa-angle-down'}></i>
-                                        </p>
-                                        <Collapse in={item.id === openId} timeout="auto" unmountOnExit>
-                                            <List className="subMenu">
-                                                <Fragment>
-                                                    {item.submenu.map((submenu, i) => {
-                                                        return (
-                                                            <ListItem key={i}>
-                                                                <Link onClick={ClickHandler} className="active"
-                                                                    href={submenu.link}>{submenu.title}</Link>
-                                                            </ListItem>
-                                                        )
-                                                    })}
-                                                </Fragment>
-                                            </List>
-                                        </Collapse>
-                                    </Fragment>
-                                    : <Link className="active"
-                                        href={item.link}>{item.title}</Link>
-                                }
-                            </ListItem>
-                        )
-                    })}
-                </ul>
-
-            </div>
-
-            <div className="showmenu" onClick={() => setMenuState(!menuActive)}>
-                <button type="button" className="navbar-toggler open-btn">
-                    <span className="icon-bar first-angle"></span>
-                    <span className="icon-bar middle-angle"></span>
-                    <span className="icon-bar last-angle"></span>
-                </button>
-            </div>
+  return (
+    <div className="mobail_menu">
+      <div className={`mobileMenu ${menuActive ? "show" : ""}`}>
+        <div className="menu-close">
+          <div className="clox" onClick={() => setMenuState(!menuActive)}>
+            <i className="ti-close"></i>
+          </div>
         </div>
-    )
-}
+
+        <ul className="responsivemenu">{renderMenuItems(menuStructure)}</ul>
+      </div>
+
+      <div className="showmenu" onClick={() => setMenuState(!menuActive)}>
+        <button type="button" className="navbar-toggler open-btn">
+          <span className="icon-bar first-angle"></span>
+          <span className="icon-bar middle-angle"></span>
+          <span className="icon-bar last-angle"></span>
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default MobileMenu;
